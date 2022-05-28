f28d883fad741667600fcd61ca26c8f0e269a08e
paras <parasverma2531@gmail.com>
2022-05-28 23:05:00 +0530

    Object method successfully worked

--- ProjectMain.java
+++ ProjectMain.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 import LinkedListProject.ReferenceFunctionsClass;
+import LinkedListProject.SinglyList;
 public class ProjectMain {
 
     public static void main(String[] args) {
@@ -7,23 +8,26 @@ public class ProjectMain {
         Scanner in= new Scanner(System.in);
         int choice;
         System.out.println("'1' for SinglyLinkedList\n'2' for doublyLinkedList\n'3' for circularLinkedList");
-        choice=in.nextInt();
+        choice=in.nextInt();Boolean val =true;
         switch (choice)
         {
             case 1:
-               rf.DisplayDetails();
-               choice=in.nextInt();
-               rf.PerformOperations(choice);
+                SinglyList sl= new SinglyList();
+                while (val) {
+                    rf.DisplayDetails();
+                    choice = in.nextInt();
+                    val=rf.PerformOperations(choice,sl);
+                }
                 break;
             case 2:
                rf.DisplayDetails();
                choice=in.nextInt();
-               rf.PerformOperations(choice);
+               //rf.PerformOperations(choice,sl);
                 break;
             case 3:
                 rf.DisplayDetails();
                 choice=in.nextInt();
-                rf.PerformOperations(choice);
+                //rf.PerformOperations(choice,sl);
                 break;
             default:
                 System.out.println("enter valid choice");
