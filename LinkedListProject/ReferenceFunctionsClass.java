5d75ea56c5bee85fd89bc10e704babc8f3272509
paras <parasverma2531@gmail.com>
2022-05-28 22:08:32 +0530

    ReferenceFunctionClass

---
+++ LinkedListProject/ReferenceFunctionsClass.java
@@ -0,0 +1,8 @@
+package LinkedListProject;
+
+public class ReferenceFunctionsClass {
+    public  void DisplayDetails()
+    {
+        System.out.println("'1' to add at the beginning\n'2' to add at the end\n'3' to add at the position\n'4' to delete from the beginning\n'5' to delete from end\n'6' to delete from position\n'7' to reverse the list");
+    }
+}
