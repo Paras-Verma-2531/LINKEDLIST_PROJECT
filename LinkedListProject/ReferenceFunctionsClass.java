78a5937543578722dd2ceb4901d6d75f7f6c3d0c
paras <parasverma2531@gmail.com>
2022-05-28 22:39:29 +0530

    modified

--- LinkedListProject/ReferenceFunctionsClass.java
+++ LinkedListProject/ReferenceFunctionsClass.java
@@ -1,37 +1,56 @@
 package LinkedListProject;
+import java.util.Scanner;
 
 public class ReferenceFunctionsClass {
     public  void DisplayDetails()
     {
-        System.out.println("'1' to add at the beginning\n'2' to add at the end\n'3' to add at the position\n'4' to delete from the beginning\n'5' to delete from end\n'6' to delete from position\n'7' to reverse the list");
+        System.out.println("'1' to add at the beginning\n'2' to add at the end\n'3' to add at the position\n'4' to delete from the beginning\n'5' to delete from end\n'6' to delete from position\n'7' to reverse the list\n'8' to exit");
     }
     public void PerformOperations(int x)
     {
-        switch (x)
-        {
-            case 1:
-                System.out.println("successfully done");
-                break;
-            case 2:
-                System.out.println("successfully done");
-                break;
-            case 3:
-                System.out.println("successfully done");
-                break;
-            case 4:
-                System.out.println("successfully done");
-                break;
-            case 5:
-                System.out.println("successfully done");
-                break;
-            case 6:
-                System.out.println("successfully done");
-                break;
-            case 7:
-                System.out.println("successfully done");
-                break;
-            default:
-                System.out.println("Enter valid Input");
+        Scanner in = new Scanner(System.in);
+        int data,pos,deletedData;Boolean val=true;
+        SinglyList sl =new SinglyList();
+        while(val) {
+            switch (x) {
+                case 1:
+                    System.out.print("enter the data:");
+                    data = in.nextInt();
+                    sl.addAtBeg(data);
+                    System.out.println("successfully done");
+                    break;
+                case 2:
+                    System.out.print("enter the data:");
+                    data = in.nextInt();
+                    sl.addAtEnd(data);
+                    System.out.println("successfully done");
+                    break;
+                case 3:
+                    System.out.print("enter the data:");
+                    data = in.nextInt();
+                    System.out.print("Enter the position:");
+                    pos=in.nextInt();
+                    sl.addAtPos(pos,data);
+                    System.out.println("successfully done");
+                    break;
+                case 4:
+                    System.out.println("successfully done");
+                    break;
+                case 5:
+                    System.out.println("successfully done");
+                    break;
+                case 6:
+                    System.out.println("successfully done");
+                    break;
+                case 7:
+                    System.out.println("successfully done");
+                    break;
+                case 8:
+                    val=false;
+                default:
+                    System.out.println("Enter valid Input");
+            }
         }
     }
+
 }
