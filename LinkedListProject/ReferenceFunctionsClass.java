1ad06f704b90d4d1af0aa9a5fc112f5d9f37c122
paras <parasverma2531@gmail.com>
2022-05-28 22:16:29 +0530

    modified

--- LinkedListProject/ReferenceFunctionsClass.java
+++ LinkedListProject/ReferenceFunctionsClass.java
@@ -5,4 +5,33 @@ public class ReferenceFunctionsClass {
     {
         System.out.println("'1' to add at the beginning\n'2' to add at the end\n'3' to add at the position\n'4' to delete from the beginning\n'5' to delete from end\n'6' to delete from position\n'7' to reverse the list");
     }
+    public void PerformOperations(int x)
+    {
+        switch (x)
+        {
+            case 1:
+                System.out.println("successfully done");
+                break;
+            case 2:
+                System.out.println("successfully done");
+                break;
+            case 3:
+                System.out.println("successfully done");
+                break;
+            case 4:
+                System.out.println("successfully done");
+                break;
+            case 5:
+                System.out.println("successfully done");
+                break;
+            case 6:
+                System.out.println("successfully done");
+                break;
+            case 7:
+                System.out.println("successfully done");
+                break;
+            default:
+                System.out.println("Enter valid Input");
+        }
+    }
 }
