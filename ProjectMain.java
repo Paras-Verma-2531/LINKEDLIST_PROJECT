286be6c2bcb315b598d74fca1f572d7201cc645a
paras <parasverma2531@gmail.com>
2022-08-04 14:27:15 +0530

    ProjectMain

--- ProjectMain.java
+++ ProjectMain.java
@@ -4,34 +4,39 @@ import LinkedListProject.SinglyList;
 public class ProjectMain {
 
     public static void main(String[] args) {
-        ReferenceFunctionsClass rf= new ReferenceFunctionsClass();
-        Scanner in= new Scanner(System.in);
+        ReferenceFunctionsClass rf = new ReferenceFunctionsClass();
+        Scanner in = new Scanner(System.in);
         int choice;
-        System.out.println("'1' for SinglyLinkedList\n'2' for doublyLinkedList\n'3' for circularLinkedList");
-        choice=in.nextInt();Boolean val =true;
-        switch (choice)
-        {
-            case 1:
-                SinglyList sl= new SinglyList();
-                while (val) {
+        Boolean check = true;
+        while (check) {
+            System.out.println("'1' for SinglyLinkedList\n'2' for doublyLinkedList\n'3' for circularLinkedList\n'4' to exit");
+            choice = in.nextInt();
+            Boolean val = true;
+            switch (choice) {
+                case 1:
+                    SinglyList sl = new SinglyList();
+                    while (val) {
+                        rf.DisplayDetails();
+                        choice = in.nextInt();
+                        val = rf.PerformOperations(choice, sl);
+                    }
+                    break;
+                case 2:
                     rf.DisplayDetails();
                     choice = in.nextInt();
-                    val=rf.PerformOperations(choice,sl);
-                }
-                break;
-            case 2:
-               rf.DisplayDetails();
-               choice=in.nextInt();
-               //rf.PerformOperations(choice,sl);
-                break;
-            case 3:
-                rf.DisplayDetails();
-                choice=in.nextInt();
-                //rf.PerformOperations(choice,sl);
-                break;
-            default:
-                System.out.println("enter valid choice");
+                    //rf.PerformOperations(choice,sl);
+                    break;
+                case 3:
+                    rf.DisplayDetails();
+                    choice = in.nextInt();
+                    //rf.PerformOperations(choice,sl);
+                    break;
+                case 4:
+                    check=false;
+                default:
+                    System.out.println("enter valid choice");
 
+            }
         }
     }
 }
